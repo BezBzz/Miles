757313ac63251b8aeab9e492deaf90cc41acb11f
BezBzz <apaser90@mail.ru>
2022-03-17 23:12:52 +0300

    First Commit

---
+++ src/main.java
@@ -0,0 +1,12 @@
+public class main {
+    public static void main(String[] args) {
+        // вводные данные
+        int price = 600;
+        int miles = 20;
+        // логика
+        System.out.println("Сумма бонуса составит: ");
+        System.out.println(price / miles);
+
+
+    }
+}
